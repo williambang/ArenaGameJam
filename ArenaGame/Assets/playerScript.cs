74afc4ef1b83808234314a5c24831b82ea6d5d4d
Idunsj <43698781+Idunsj@users.noreply.github.com>
2018-09-29 16:02:25 +0200

    Payer Changed

--- ArenaGame/Assets/playerScript.cs
+++ ArenaGame/Assets/playerScript.cs
@@ -10,23 +10,6 @@ public class playerScript : MonoBehaviour
         float x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
         float z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
 
-
         transform.Translate(x, 0, z);
-<<<<<<< HEAD
-        //moveplayer();
-    }
-
-
-    void moveplayer()
-    {
-        Debug.Log("scream");
-=======
-        MovePlayer();
-    }
-
-    void MovePlayer()
-    {
-        Debug.Log("Blabladick");
->>>>>>> d0fa94580aa23e488a44c17978d21fafc981771b
     }
 }

29083b025d0c96a7eb83bd1aff53e2ed947c6e87
Ollebull <43698785+Ollebull@users.noreply.github.com>
2018-09-29 17:57:23 +0200

    Test_001

---
+++ ArenaGame/Assets/OlleScript.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+public class OlleScript : MonoBehaviour
+{
+	public float speed;
+	public int health;
+	public string playerName;
+    public GameObject DeflectAbility;
+	public Transform player;
+
+	void Start() {
+	Instantiate(DeflectAbility, player.position, player.rotation, player);
+	}
+
+    private void Instantiate(GameObject deflectAbility, object position, object rotation, Transform player)
+    {
+        throw new NotImplementedException();
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        float z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+        transform.Translate(x, 0, z);
+    }
+
+	public void takeDamage(int damage) {
+
+		health = health - damage;
+
+	}
+
+   // void Deflect() {
+	//	Instantiate(OlleEffect, player.position, player.rotation, player);
+	//}
+}

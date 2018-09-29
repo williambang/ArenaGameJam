76c3f45acd82bfde392a633b047d2f83c1fee8cf
williambang <williamlarsenbang@gmail.com>
2018-09-29 14:05:04 +0200

    Arena+Scene
    Player, Arena, and Cannon

---
+++ ArenaGame/Assets/playerScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class playerScript : MonoBehaviour
+{
+	public float speed;
+	public int health;
+
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+        transform.Translate(x, 0, z);
+    }
+}

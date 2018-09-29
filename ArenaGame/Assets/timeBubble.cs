673dd221d64d86240737d9628e3a735c06dfa0b4
williambang <williamlarsenbang@gmail.com>
2018-09-29 18:19:00 +0200

    TimeBubble

---
+++ ArenaGame/Assets/timeBubble.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timeBubble : MonoBehaviour {
+
+	// Use this for initialization
+
+	public float speedDecrease = 3.0f;
+
+	void Start () {
+		
+	}
+	
+	void OnTriggerEnter(Collider other) {
+
+		if(other.tag == "ammo"){
+			other.gameObject.GetComponent<bulletScript>().speed = speedDecrease;
+		}
+
+	}
+
+}
